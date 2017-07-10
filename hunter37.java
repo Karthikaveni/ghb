602d6f786f8a0ed90c9482619ef7a5ac8d4bdc15
Karthikaveni <karthikaveni18@gmail.com>
2017-07-10 18:06:23 +0530

    Create hunter37.java

---
+++ hunter37.java
@@ -0,0 +1,32 @@
+# ghb
+import java.io.*;
+import java.util.*;
+
+public class hunter37{
+ 
+        public static void main(String[] args) {
+               
+               
+                int lim = 100;
+               
+                System.out.println("Prime numbers between 1 and " + lim);
+               
+             
+                for(int j=1; j < 100; j++){
+                       
+                        boolean isPrime = true;
+                       
+                     
+                        for(int i=2; i < j ; i++){
+                               
+                                if(j % i == 0){
+                                        isPrime = false;
+                                        break;
+                                }
+                        }
+                      
+                        if(isPrime)
+                                System.out.print(j + " ");
+                }
+        }
+}
